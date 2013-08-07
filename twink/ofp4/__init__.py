5a832903158bbdf6e6ef2fd63abe1e244cab4e0c
Hiroaki KAWAI <hiroaki.kawai@gmail.com>
2013-08-07 13:04:23 +0900

    add openflow 1.0 12-tuple parser

--- twink/ofp4/__init__.py
+++ twink/ofp4/__init__.py
@@ -1,3 +1,6 @@
+import struct
+import collections
+
 class _enum_base(object):
 	numbers = None
 	bitshifts = None
@@ -484,3 +487,54 @@ ofp_flow_expired_readon = type("ofp_flow_expired_readon", (_enum_base,), {
 	"prefix": "OFPER",
 	"numbers": "IDLE_TIMEOUT HARD_TIMEOUT"
 	})(globals())
+
+
+def ofptuple(etherframe):
+	'''
+	returns an openflow v1.0 12 tuple without the first in_port
+	'''
+	(ethernet_dst, ethernet_src, ethernet_type, tci, inner_type) = struct.unpack_from("!6s6sHHH", etherframe)
+	if ethernet_type == 0x8100:
+		vlan_id = tci&0x0FFF
+		vlan_priority = tci>>13
+		ethernet_type = inner_type
+		offset = 4
+	else:
+		vlan_id = None
+		vlan_priority = None
+		offset = 0
+	
+	if ethernet_type < 0x05DC:
+		(llc_dsap, llc_ssap, llc_ctl, snap_oui, snap_type) = struct.unpack_from("!BBB3sH", etherframe, offset=14)
+		if llc_dsap==0xAA and llc_ssap==0xAA and snap_oui=="0x00"*3:
+			ethernet_type = snap_type
+			offset = 8
+	
+	ip_tos = ip_protocol = ip_src = ip_dst = None
+	transport_src_port_or_icmp_type = None
+	transport_dst_port_or_icmp_code = None
+	if ethernet_type == 0x0800: # IP
+		(u1, ip_tos, u2, ip_protocol, u3, ip_src, ip_dst, src_port, dst_port) = struct.unpack_from("!sB7sB2s4s4sHH", etherframe, offset=14+offset)
+		if ip_protocol == 1: # ICMP
+			transport_src_port_or_icmp_type = src_port>>8
+			transport_dst_port_or_icmp_code = src_port&0xFF
+		elif ip_protocol in (6, 17): # TCP, UDP
+			transport_src_port_or_icmp_type = src_port
+			transport_dst_port_or_icmp_code = dst_port
+	elif ethernet_type == 0x0806: # ARP
+		(u1, ip_protocol, u2, ip_src, u3, ip_dst) = struct.unpack_from("!6sH6s4s6s4s", etherframe, offset=14+offset)
+	
+	return collections.namedtuple("ofptuple", "in_port dl_src dl_dst dl_type dl_vlan dl_vlan_pcp nw_src nw_dst nw_proto ip_tos tp_src tp_dst")(
+		None,
+		ethernet_src,
+		ethernet_dst,
+		ethernet_type,
+		vlan_id,
+		vlan_priority,
+		ip_src,
+		ip_dst,
+		ip_protocol,
+		ip_tos,
+		transport_src_port_or_icmp_type,
+		transport_dst_port_or_icmp_code
+		)
