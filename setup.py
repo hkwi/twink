6dd5406060a66a6f9b0d973266a50cd95901f841
Hiroaki KAWAI <hiroaki.kawai@gmail.com>
2013-12-31 21:59:53 +0900

    add distutils files

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='twink',
+        version='0.1',
+        description='Openflow library',
+        author='Hiroaki Kawai',
+        author_email='hiroaki.kawai@gmail.com',
+        url='https://github.com/hkwi/twink/',
+        packages=['twink','twink.ofp4','twink.ofp5']
+)
