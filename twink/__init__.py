b1de261cb25aea0dd88b297c72802af0ca68d943
Hiroaki KAWAI <hiroaki.kawai@gmail.com>
2014-05-14 18:57:22 +0900

    add twink.sched proxy object
    this helps easy access to serve_forever, etc.

--- twink/__init__.py
+++ twink/__init__.py
@@ -1,1 +1,10 @@
+import base
 from .base import *
+
+class _sched_proxy(object):
+	def __getattr__(self, name):
+		if name in "subprocess socket Queue Lock Event spawn serve_forever".split():
+			return getattr(base.sched, name)
+		raise AttributeError("No such attribute")
+
+sched = _sched_proxy()
