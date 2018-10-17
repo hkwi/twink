9c671d7d044007af9cd56c2b43da2cb27a76e01f
Hiroaki KAWAI <hiroaki.kawai@gmail.com>
2018-10-17 17:37:25 +0900

    add long_description

--- setup.py
+++ setup.py
@@ -1,10 +1,16 @@
-from distutils.core import setup
+import unittest
+try:
+	from setuptools import setup
+except ImportError:
+	from distutils.core import setup
 
 setup(name='twink',
         version='0.3',
         description='Openflow library',
+	long_description=open("README.md").read(),
+	long_description_content_type="text/markdown",
         author='Hiroaki Kawai',
         author_email='hiroaki.kawai@gmail.com',
         url='https://github.com/hkwi/twink/',
-        packages=['twink','twink.ofp4','twink.ofp5']
+        packages=['twink','twink.ofp4','twink.ofp5'],
 )
